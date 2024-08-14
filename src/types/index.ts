c9fce45cd31c025ada60769676213fffd3cc2c9f
ElementRef <168389155+ElementRef@users.noreply.github.com>
2024-08-14 17:11:46 +0800

    ElementRef/TsAxios

---
+++ src/types/index.ts
@@ -0,0 +1,38 @@
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'DELETE'
+  | 'options'
+  | 'OPTIONS'
+  | 'put'
+  | 'PUT'
+  | 'post'
+  | 'POST'
+  | 'head'
+  | 'HEAD'
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+  headers?: any
+  responseType?: XMLHttpRequestResponseType
+  timeout?: number
+}
+export interface AxiosResponse {
+  data: any
+  status: number
+  statusText: string
+  headers: AxiosRequestConfig
+  config: AxiosRequestConfig
+  request: any
+}
+export interface AxiosPromise extends Promise<AxiosResponse> {}
+export interface AxiosError extends Error {
+  isAxiosError: boolean
+  config: AxiosRequestConfig
+  code?: string | null
+  request?: any
+  response?: AxiosResponse
+}
